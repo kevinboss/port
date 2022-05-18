cd5a1d1e1149e50ae82261647c19c9b32e5ee3b6
Babuki <kevin.boss@outlook.com>
2022-05-18 23:16:18 +0200

    Improve download progress

--- src/Commands/Pull/PullCommand.cs
+++ src/Commands/Pull/PullCommand.cs
@@ -1,4 +1,6 @@
+using System.Reactive.Linq;
 using Docker.DotNet;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace dcma.Commands.Pull;
@@ -50,6 +52,74 @@ public class PullCommand : AsyncCommand<PullSettings>
         }
 
         var imageName = imageConfig.ImageName;
-        await _createImageCommand.ExecuteAsync(imageName, tag);
+        AnsiConsole.WriteLine($"Downloading image {DockerHelper.JoinImageNameAndTag(imageName, tag)}");
+        var tasks = new Dictionary<string, string>();
+        var lockObject = new object();
+        var table = new Table();
+        table.HideHeaders();
+        table.Border = TableBorder.None;
+        table.AddColumn("Downloads");
+        await AnsiConsole.Live(table)
+            .StartAsync(async ctx =>
+            {
+                using (_createImageCommand.ProgressObservable
+                           .Subscribe(progress =>
+                           {
+                               lock (lockObject)
+                               {
+                                   var value =
+                                       progress.ProgressMessage == null
+                                           ? progress.Description.EscapeMarkup()
+                                           : $"{progress.Description} {progress.ProgressMessage}".EscapeMarkup();
+                                   if (progress.Initial)
+                                   {
+                                       tasks.Add(progress.Id, value);
+                                       table.AddRow(value);
+                                   }
+                                   else
+                                   {
+                                       tasks.Remove(progress.Id);
+                                       tasks.Add(progress.Id, value);
+                                   }
+
+                                   var row = 0;
+                                   foreach (var task in tasks)
+                                   {
+                                       table.UpdateCell(row, 0, task.Value);
+                                       row++;
+                                   }
+
+                                   ctx.Refresh();
+                               }
+                           }))
+                {
+                    await _createImageCommand.ExecuteAsync(imageName, tag);
+                }
+            });
+
+        AnsiConsole.WriteLine($"Image {DockerHelper.JoinImageNameAndTag(imageName, tag)} downloaded");
+    }
+
+    private IDisposable SubscribeToProgressObserable(object lockObject, IDictionary<string, string> tasks)
+    {
+        return _createImageCommand.ProgressObservable
+            .Subscribe(progress =>
+            {
+                lock (lockObject)
+                {
+                    if (progress.Initial)
+                        tasks.Add(progress.Id, $"{progress.Description} {progress.ProgressMessage}");
+                    else
+                    {
+                        tasks.Remove(progress.Id);
+                        tasks.Add(progress.Id, $"{progress.Description} {progress.ProgressMessage}");
+                    }
+
+                    foreach (var keyValuePair in tasks.OrderBy(e => e.Key))
+                    {
+                        AnsiConsole.Write($"{keyValuePair.Value}");
+                    }
+                }
+            });
     }
 }
---
+++ src/Progress.cs
@@ -0,0 +1,9 @@
+namespace dcma;
+
+public class Progress
+{
+    public bool Initial { get; set; }
+    public string Id { get; set; }
+    public string Description { get; set; }
+    public string? ProgressMessage { get; set; }
+}
