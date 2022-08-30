094cd19b9e1e0508eb6bc1dc7ccd40cf58aea171
Kevin Boss <kevin.boss@outlook.com>
2022-08-30 14:22:08 +0200

    Local datetime in UI ausgeben

--- src/TagTextBuilder.cs
+++ src/TagTextBuilder.cs
@@ -7,28 +7,37 @@ public static class TagTextBuilder
 {
     public static string BuildTagText(Image image)
     {
-        var imageTypeText = image.IsSnapshot ? "Snapshot" : "Base";
-        var sb = new StringBuilder($"[white]{image.Tag ?? "<none>".EscapeMarkup()}");
-        sb.Append($" ({imageTypeText})[/]");
+        var sb = new StringBuilder();
+        sb.Append($"[white]{image.Tag ?? "<none>".EscapeMarkup()}");
+        sb.Append(" (");
+        if (image.IsSnapshot)
+        {
+            sb.Append("Snapshot");
+            if (image.Parent != null)
+                sb.Append($" based on {image.Parent.Tag ?? "[orange3]untagged image[/]"}");
+        }
+        else
+            sb.Append("Base");
+        sb.Append("[/]");
+        var imageCreated = image.Created?.ToLocalTime();
         switch (image.IsSnapshot)
         {
             case false when !image.Existing:
-                sb.Append(" [red]missing[/]");
+                sb.Append(" / [red]missing[/]");
                 break;
             case false when image.Existing:
-                sb.Append($" ({image.Created.ToString()})");
+                sb.Append($" / {imageCreated.ToString()}");
                 break;
             case true:
-                sb.Append($" ({image.Created.ToString()})");
+                sb.Append($" / {imageCreated.ToString()}");
                 break;
         }
+        sb.Append(')');
 
         if (image.Running && !image.RelatedContainerIsRunningUntaggedImage)
-            sb.Append($" [green]running[/]");
+            sb.Append(" [green]running[/]");
         if (image.Running && image.RelatedContainerIsRunningUntaggedImage)
             sb.Append(" (running [orange3]untagged image[/])");
-        if (image.Parent != null)
-            sb.Append($" (based on {image.Parent.Tag ?? "[orange3]untagged image[/]"})");
 
         return sb.ToString();
     }
