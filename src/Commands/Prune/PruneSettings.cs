d7cc0d68434a8abf860b0fcabefbb2cbcab7da06
Boss Kevin <kevin.boss@kms-ag.ch>
2026-03-27 10:38:00 +0100

    feat: reimplement prune command for dangling image cleanup

--- src/Commands/Prune/PruneSettings.cs
+++ src/Commands/Prune/PruneSettings.cs
@@ -2,4 +2,8 @@ using Spectre.Console.Cli;
 
 namespace port.Commands.Prune;
 
-internal class PruneSettings : CommandSettings;
+internal class PruneSettings : CommandSettings
+{
+    [CommandArgument(0, "[ImageIdentifier]")]
+    public string? ImageIdentifier { get; set; }
+}
